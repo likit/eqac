94b03e830793543328fb26f16d5f600881aae48a
Jareanporn Julchoo <Famp@Jareanporns-MacBook-Air.local>
2016-11-10 17:55:10 +0700

    added alt test
    added few more methods

--- warehouse-dump.sql
+++ warehouse-dump.sql
@@ -1,58 +1,39 @@
 --
--- File generated with SQLiteStudio v3.1.0 on Tue Nov 8 23:10:29 2016
+-- File generated with SQLiteStudio v3.1.1 on Thu Nov 10 17:54:11 2016
 --
 -- Text encoding used: UTF-8
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Table: tests
-DROP TABLE IF EXISTS tests;
-
-CREATE TABLE tests (
+-- Table: customers
+CREATE TABLE customers (
     id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    cid  INTEGER UNIQUE
+                 NOT NULL,
     name TEXT    UNIQUE
                  NOT NULL
 );
 
-INSERT INTO tests (
-                      id,
-                      name
-                  )
-                  VALUES (
-                      1,
-                      'alp'
-                  );
-
-INSERT INTO tests (
-                      id,
-                      name
-                  )
-                  VALUES (
-                      2,
-                      'albumin'
-                  );
-
-
--- Table: trial_sum
-DROP TABLE IF EXISTS trial_sum;
-
-CREATE TABLE trial_sum (
-    id        INTEGER PRIMARY KEY AUTOINCREMENT,
-    test_id   INTEGER REFERENCES tests (id),
-    method_id INTEGER REFERENCES methods (id),
-    sd        DECIMAL,
-    cv        DECIMAL,
-    mean      DECIMAL,
-    new_sd    DECIMAL,
-    new_cv    DECIMAL,
-    new_mean  DECIMAL
+-- Table: methods
+CREATE TABLE methods (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT    UNIQUE
+                 NOT NULL
 );
-
+INSERT INTO methods (id, name) VALUES (1, 'vitros');
+INSERT INTO methods (id, name) VALUES (2, 'bcg');
+INSERT INTO methods (id, name) VALUES (3, 'bcp');
+INSERT INTO methods (id, name) VALUES (4, 'IFCC');
+INSERT INTO methods (id, name) VALUES (5, 'AACC');
+INSERT INTO methods (id, name) VALUES (6, 'DGKC');
+INSERT INTO methods (id, name) VALUES (7, 'Beckman');
+INSERT INTO methods (id, name) VALUES (8, 'REFLO');
+INSERT INTO methods (id, name) VALUES (9, 'DADE');
+INSERT INTO methods (id, name) VALUES (10, 'KE-wout-pyridoxal');
+INSERT INTO methods (id, name) VALUES (11, 'KE-pyridoxal');
 
 -- Table: results
-DROP TABLE IF EXISTS results;
-
 CREATE TABLE results (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     test_id     INTEGER REFERENCES tests (id),
@@ -64,100 +45,28 @@ CREATE TABLE results (
     ccv         DECIMAL
 );
 
-
--- Table: customers
-DROP TABLE IF EXISTS customers;
-
-CREATE TABLE customers (
-    id   INTEGER PRIMARY KEY AUTOINCREMENT,
-    cid  INTEGER UNIQUE
-                 NOT NULL,
-    name TEXT    UNIQUE
-                 NOT NULL
-);
-
-
--- Table: methods
-DROP TABLE IF EXISTS methods;
-
-CREATE TABLE methods (
+-- Table: tests
+CREATE TABLE tests (
     id   INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT    UNIQUE
                  NOT NULL
 );
+INSERT INTO tests (id, name) VALUES (1, 'alp');
+INSERT INTO tests (id, name) VALUES (2, 'albumin');
+INSERT INTO tests (id, name) VALUES (3, 'alt');
 
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        1,
-                        'vitros'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        2,
-                        'bcg'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        3,
-                        'bcp'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        4,
-                        'IFCC'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        5,
-                        'AACC'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        6,
-                        'DGKC'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        7,
-                        'Beckman'
-                    );
-
-INSERT INTO methods (
-                        id,
-                        name
-                    )
-                    VALUES (
-                        8,
-                        'REFLO'
-                    );
-
+-- Table: trial_sum
+CREATE TABLE trial_sum (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    test_id   INTEGER REFERENCES tests (id),
+    method_id INTEGER REFERENCES methods (id),
+    sd        DECIMAL,
+    cv        DECIMAL,
+    mean      DECIMAL,
+    new_sd    DECIMAL,
+    new_cv    DECIMAL,
+    new_mean  DECIMAL
+);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
